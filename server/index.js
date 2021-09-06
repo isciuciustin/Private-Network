5c6d320ba39f72110139757bc5f03b7bc6fb254a
isciuciustin <iustinconstantin001@gmail.com>
2021-09-06 17:45:28 +0300

    Rooms

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
-var users = [];
+var users = {};
 var cnt = 0;
 app.get("/", (req, res) => {
     res.sendFile('index.html', { root: '../client/' });
@@ -14,25 +14,25 @@ app.get("/", (req, res) => {
 app.get('/room/:url', (req, res) => {
     const url = req.params.url;
     res.sendFile('room.html', { root: '../client/' });
+});
 
-    io.on('connection', (socket) => {
-            cnt++;
-            users.push(socket.id);
-            console.log(cnt + " users on");
-            socket.on('disconnect', () => {
-                cnt--;
-                console.log(cnt + " users remained");
-            });
-    })
-    io.on('connection', (socket) => {
-            socket.on('chat message', (msg, room) => {
-
-                io.to(room).emit("chat message", `{${msg}}`);
-            });
-            socket.on('join-room', (room) => {
-                socket.join(room);
-            })
+io.on('connection', (socket) => {
+    cnt++;
+    users[socket.id] = socket;
+    console.log(cnt + " users on" + socket.id);
+    socket.on('disconnect', () => {
+        cnt--;
+        console.log(cnt + " users remained");
     });
+})
+io.on('connection', (socket) => {
+    socket.on('chat message', (msg, room) => {
+
+        io.to(room).emit("chat message", `{${msg}}`);
+    });
+    socket.on('join-room', (room) => {
+        socket.join(room);
+    })
 });
 
 server.listen(8080, () => {
